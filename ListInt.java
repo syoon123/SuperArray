f30c07b39475bb7973c9d98b7e254c0dbd2af9c0
Sarah Yoon <syoon2@stuy.edu>
2015-12-04 15:29:26 -0500

    added comments to ListInt

--- ListInt.java
+++ ListInt.java
@@ -4,10 +4,21 @@
    2015-12-04 */
 
 public interface ListInt {
+    //adds item at index, shifts existing elements to the right
     void add(int index, int newVal);
+
+    //adds item after last item
     void add(int newVal);
+
+    //removes specifie item and shifts array, changes size and lastpos accordingly
     void remove(int index);
+
+    //returns the number of meaningful elements
     int size();
+
+    //changes value of element at index to newVal
     int set(int index, int newVal);
+
+    //returns the value of the element at index
     int get(int index );
 }
